a08c45d3d3248d901fca61d18f8b7a1b5ed8e076
ibrahimijc <33429696+ibrahimijc@users.noreply.github.com>
2020-03-26 19:31:29 +0500

    changed server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public/'));
-app.listen(process.env.PORT || 8080);
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});app.listen(port);
